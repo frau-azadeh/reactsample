cf738faa0b6da1bdc0f9741657779d6d53e9b6a9
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2024-12-17 03:50:21 -0800

    update pagination

--- src/app/component/pagination/Pagination.tsx
+++ src/app/component/pagination/Pagination.tsx
@@ -8,8 +8,27 @@ export const Pagination:React.FC<PaginationProps> = ({
     postsPerPage,
 
 }) => {
-
+const pageNumbers = [];
+for(let i=1 ; i<=Math.ceil(totalPosts /postsPerPage);i++){
+  pageNumbers.push(i);
+}
   return (
-    <div>Pagination</div>
+    <div>
+      <nav className='flex justify-center items-center space-x-2'>
+        {pageNumbers.map((number, index)=>(
+          <button
+            onClick={(e)=>paginate(number)}
+            key={index}
+            className={`px-4 py-2 border rounded-lg ${
+              currentPage === number
+              ? "bg-blue-500 text-white"
+              : "bg-gray-100 text-gray-700 hover:bg-gray-200"
+
+            }transition`}
+          >{number}</button>
+        ))}
+      </nav>
+     
+    </div>
   )
 }
