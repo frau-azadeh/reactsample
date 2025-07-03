dae065774aa10b635754aa1a05d2e9370ca2765c
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-03 21:19:47 +0330

    delete

--- src/api/userApi.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "@/types/userTypes";
-import axios from "axios";
-
-const BAS_URL = "https://jsonplaceholder.typicode.com/users";
-
-export const getUsers = async () : Promise <User[]> =>{
-    const response = await axios.get<User[]>(BAS_URL);
-    return response.data;
-};
