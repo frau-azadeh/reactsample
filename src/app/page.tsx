03823c632ab666ab41832b522a0eacc9371fe2a4
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-04 22:37:11 +0330

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-blue-600">hi my best</h1>
-    </div>
-  );
+'use client'
+
+import { useUser } from '../hook/useUser'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+export default function HomePage() {
+  const user = useUser()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (user) router.replace('/profile')
+    else router.replace('/auth')
+  }, [user, router])
+
+  return null
 }
